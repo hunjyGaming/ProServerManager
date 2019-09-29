8fc56429b1cab1200a997bfd585a044384d640d0
masterbuilders <L0y8NxJXAv52Eo1R>
2019-09-29 15:52:45 +0200

    reload Command

---
+++ src/de/hunjy/commands/CMD_reload.java
@@ -0,0 +1,11 @@
+package de.hunjy.commands;
+/*
+    Create by RiedCrafter on 29.09.2019
+    @author: RiedCrafter
+    @date: 29.09.2019
+    @time: 15:52
+    @projekt: ProServerManager
+*/
+
+public class CMD_reload {
+}
