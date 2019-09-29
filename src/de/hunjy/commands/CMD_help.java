f2d3751a5bacf5f3a70c0af40b8280c8db65e70f
hunjy <48495704+hunjy@users.noreply.github.com>
2019-09-29 15:48:28 +0200

    update

---
+++ src/de/hunjy/commands/CMD_help.java
@@ -0,0 +1,11 @@
+package de.hunjy.commands;
+
+/*
+    Create by hunjy on 29.09.2019
+    @auther: hunjy
+    @date: 29.09.2019
+    @time: 15:48
+    @projekt: ProServerManager
+*/
+public class CMD_help {
+}
