33e3cd5acb4bec2dc19f3b6667b1e746d183a468
hunjy <48495704+hunjy@users.noreply.github.com>
2019-09-29 15:09:48 +0200

    MSGManager

--- src/de/hunjy/PSM.java
+++ src/de/hunjy/PSM.java
@@ -1,5 +1,7 @@
 package de.hunjy;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /*
     Create by hunjy on 29.09.2019
     @auther: hunjy
@@ -7,5 +9,32 @@ package de.hunjy;
     @time: 14:43
     @projekt: ProServerManager
 */
-public class PSM {
+public class PSM extends JavaPlugin {
+
+    static PSM instance;
+
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        initCommands();
+        initListener();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    private void initCommands() {
+
+    }
+    private void initListener() {
+
+    }
+
+    public static PSM getInstance() {
+        return instance;
+    }
 }
---
+++ src/de/hunjy/manager/MessageManager.java
@@ -0,0 +1,56 @@
+package de.hunjy.manager;
+
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+
+/*
+    Create by hunjy on 29.09.2019
+    @auther: hunjy
+    @date: 29.09.2019
+    @time: 14:48
+    @projekt: ProServerManager
+*/
+public class MessageManager {
+
+    private File path;
+    private String pathString;
+    private long lastUpdate;
+
+    public MessageManager(String pathString) {
+        this.pathString = pathString;
+        this.path = new File(pathString);
+    }
+
+
+    private boolean isFileUpdated() {
+        long timeStamp = path.lastModified();
+
+        if( this.lastUpdate != timeStamp ) {
+            this.lastUpdate = timeStamp;
+            return true;
+        }
+        return false;
+    }
+
+    public StringBuilder get(String key) {
+
+        if(isFileUpdated()) {
+            this.path = null;
+            this.path = new File(pathString);
+        }
+
+        try {
+            JSONParser jsonParser = new JSONParser();
+            Object parsed = jsonParser.parse(this.path.getPath());
+            JSONObject jsonObject = (JSONObject)parsed;
+            return new StringBuilder().append(jsonObject.get(key));
+        }catch (ParseException e) {
+            return null;
+        }
+    }
+
+}
